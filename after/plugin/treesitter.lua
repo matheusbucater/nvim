ad8da6591ef538fe2a08722890ac4bac52fb4c4f
Matheus Pelegrini Bucater <matheusbucater@ufu.br>
2024-01-26 12:13:28 -0300

    ocaml treesitter

--- after/plugin/treesitter.lua
+++ after/plugin/treesitter.lua
@@ -6,7 +6,7 @@
   ---- If you need to change the installation directory of the parsers (see -> Advanced Setup)
   -- parser_install_dir = "/some/path/to/store/parsers", -- Remember to run vim.opt.runtimepath:append("/some/path/to/store/parsers")!
   -- A list of parser names, or "all" (the five listed parsers should always be installed)
-  ensure_installed = { "javascript", "typescript", "c", "lua", "vim", "vimdoc", "query" },
+  ensure_installed = { "javascript", "typescript", "c", "lua", "vim", "vimdoc", "query" , "ocaml", "ocaml_interface" },
 
   -- Install parsers synchronously (only applied to `ensure_installed`)
   sync_install = false,
