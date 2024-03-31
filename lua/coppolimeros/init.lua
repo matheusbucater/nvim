9a88aecde74df8718f16982da64662d7467520ef
matheusbucater <matheusp.bucater@gmail.com>
2024-03-31 15:59:42 -0300

    highlight on yank

--- lua/coppolimeros/init.lua
+++ lua/coppolimeros/init.lua
@@ -1,3 +1,11 @@
 require("coppolimeros.remap")
 require("coppolimeros.set")
 require("coppolimeros.packer")
+
+vim.api.nvim_create_autocmd('TextYankPost', {
+    desc = 'Highlight on yank',
+    group = vim.api.nvim_create_augroup('highlight_yank', { clear = true }),
+    callback = function()
+        vim.highlight.on_yank()
+    end
+})
